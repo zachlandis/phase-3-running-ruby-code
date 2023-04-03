5fff61a5d0f7b56ebabcf23033466cb796c189d0
Zach <zachlandis91@gmail.com>
2023-04-03 10:35:45 -0600

    commit tree 712b0c186f8d3b364ebcf6801cdcddcb6b7f40f7

--- app.rb
+++ app.rb
@@ -1,23 +1,5 @@
-# pp [{ id: 1, hello: "World" }, 
-#     { id: 2, hello: "Ruby" }, 
-#     { id: 3, hello: "Moon" }, 
-#     { id: 4, hello: "Learner" }]
+puts "Hello World!"
 
-# p [1,2,3]
-# puts "Hello World!"
-# print "Pass this test, please."
-# p [1,2,3]
+print "Pass this test, please."
 
-# def add
-    
-# end
-# # 1 + 1
-# puts add + add
-
-def add num1, num2
-    puts num1
-    puts num2
-    num1 + num2
-end
-
-puts add(1,3) + add(2,2)
+p [1,2,3]
